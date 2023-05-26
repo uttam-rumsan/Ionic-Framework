11e9cbf1d1b4f9ba2b3e8e26744c466c4febd8b0
UttamRajGiri <uttam.giri@rumsan.net>
2023-05-26 07:40:53 +0545

    cleanup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Blanks</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
