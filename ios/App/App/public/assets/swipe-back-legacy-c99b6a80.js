9f7bacb7167a7f30149b83e85e9aaf474bf0ebb9
UttamRajGiri <uttam.giri@rumsan.net>
2023-11-10 10:26:23 +0545

    capgo

--- ios/App/App/public/assets/swipe-back-legacy-c99b6a80.js
+++ ios/App/App/public/assets/swipe-back-legacy-c99b6a80.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ffe86fbe.js"],(function(t,e){"use strict";var r,n,o;return{setters:[t=>{r=t.G,n=t.i,o=t.c}],execute:function(){
+System.register(["./index-legacy-7b33142d.js"],(function(t,e){"use strict";var r,n,o;return{setters:[t=>{r=t.G,n=t.i,o=t.c}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { CapacitorUpdater } from '@capgo/capacitor-updater';
+
+CapacitorUpdater.notifyAppReady();
+
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
